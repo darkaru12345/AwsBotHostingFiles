ec24a54cb9a96bd03ad9c2f97e9ffd173a8f46dc
darkaru12345 <78460350+darkaru12345@users.noreply.github.com>
2021-02-03 19:12:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
-  prefix: "-",
+  prefix: "!",
   color: "#00e059",
-  token: "token here lol",
-  weez: ""
+  token: "ODA1OTgxMzk2NjAyMzIyOTU1.YBiyqw.MzQq8XmP9n3QZzdlMnOpciUvHmc",
+  weez: "poro"
  // El prefix de su bot para indentificar sus comandos.
 
 }
